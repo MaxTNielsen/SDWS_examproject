ecd217274878c6a4aab3550460a01a4bfb5aa0ef
MaxNielsen <maxtnielsen@gmail.com>
2021-01-13 11:55:48 +0100

    Design pattern in AccountManager

--- code-with-quarkus/src/main/java/com/Client/Customer.java
+++
@@ -1,8 +0,0 @@
-package com.Client;
-
-public class Customer extends User {
-    public Customer(String ID)
-    {
-        super(ID);
-    }
-}
--- code-with-quarkus/src/main/java/org/AccountManager/MerchantsResource.java
+++ code-with-quarkus/src/main/java/org/AccountManager/MerchantsResource.java
@@ -1,13 +1,10 @@
 package org.AccountManager;
 
-import com.Client.Merchant;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
