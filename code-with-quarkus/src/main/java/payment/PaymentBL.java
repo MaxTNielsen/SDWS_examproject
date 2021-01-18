54cf2f33b4fe4631c151e946ed12775a85507f3d
s184443 <62697354+s184443@users.noreply.github.com>
2021-01-18 13:24:48 +0100

    Removed token validation from payment logic

--- code-with-quarkus/src/main/java/payment/PaymentBL.java
+++ code-with-quarkus/src/main/java/payment/PaymentBL.java
@@ -11,34 +11,32 @@ public class PaymentBL {
 	SOAPPort soap = new SOAPPort();
 	MessageParsingPort msg = new MessageParsingPort();
 	
+	public PaymentBL(){
+		try {
+			msg.listen();
+			//msg.requestTokenValidation(null);
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
-	public void makeTransaction( Transaction t) {
+	
+	public boolean makeTransaction(Transaction t) {
 		if (t.getAmount() > 0) {
 			soap.TransferMoney(t);
+			if (t.isApproved()) return true;
+			else return false;
 		}
+		return false;
 	}
 	
 	
-	public boolean paymentRequest(Transaction t) throws TimeoutException {
-		TokenValidationRequest requestMessage = new TokenValidationRequest(t.getToken());
-		requestMessage.setToken(t.getToken());
-		Event ev = new Event("TOKEN_VALIDATION_REQUEST", new Object[] { requestMessage });
-		Gson gson = new Gson();
-		String requestString = gson.toJson(ev);
-		String response = msg.requestTokenValidation(requestString);
-		
-	    Event responseEvent = gson.fromJson(response ,Event.class);
-	    String jsonString3 = gson.toJson(responseEvent.getArguments()[0]);
-        TokenValidationResponse tokenResponse = gson.fromJson(jsonString3,TokenValidationResponse.class);
-        if (tokenResponse.isValid()) {
-        	t.setCustomId(tokenResponse.getCustomerId());
-        	makeTransaction(t);
-        	if (t.isApproved()) return true;
-        	else return false;
-        }
-        else return false;
-	}
 
 	
 
