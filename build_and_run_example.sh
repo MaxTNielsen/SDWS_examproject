8b9f4b9c5d1982836f2cfba709ed301a27500796
Yifei <yemi.ding@gmail.com>
2021-01-19 16:31:56 +0100

    modify script

--- build_and_run_example.sh
+++ build_and_run_example.sh
@@ -5,18 +5,22 @@ set -e
 
 # Build the services
 pushd token-service
+chmod +x ./build.sh
 ./build.sh
 popd 
 
 pushd payment-service
+chmod +x ./build.sh
 ./build.sh
 popd 
 
 # Update the set of services and
 # build and execute the system tests
 pushd demo_client
+chmod +x ./deploy.sh
 ./deploy.sh 
 sleep 20s
+chmod +x ./test.sh
 ./test.sh
 popd
 
