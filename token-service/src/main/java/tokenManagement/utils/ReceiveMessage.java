23c2bdefaebb67ef16ee4ea783a356c8ff16150d
Yifei <yemi.ding@gmail.com>
2021-01-19 15:48:46 +0100

    add token service project

---
+++ token-service/src/main/java/tokenManagement/utils/ReceiveMessage.java
@@ -0,0 +1,29 @@
+package tokenManagement.utils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class ReceiveMessage {
+    private static final String EXCHANGE_NAME = "MICROSERVICES_EXCHANGE";
+    private static final String QUEUE_TYPE = "topic";
+    private static final String TOPIC = "*.response";
+
+    //for testing 
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, QUEUE_TYPE);
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, TOPIC);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
